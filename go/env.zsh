f58c66d7b47cc68acee2efe9e6b2a0dcc5219afb
Alexej Kubarev <alexej.kubarev@accedo.tv>
2015-11-05 14:08:45 +0100

    moving gopath to hidden

--- go/env.zsh
+++ go/env.zsh
@@ -1,6 +1,12 @@
 
-# GOPATH
-export GOPATH=$HOME/Personal/go
+# GOPATH (it's hidden, we try to use gb)
+export GOPATH=$HOME/.go
 
 # Add Go to PATH
 export PATH=$PATH:/usr/local/opt/go/libexec/bin:$GOPATH/bin
+
+# Expose to GUI (on mac)
+if [[ $OSTYPE == darwin* ]];
+then
+    launchctl setenv GOPATH "$GOPATH"
+fi
