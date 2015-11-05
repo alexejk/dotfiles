725e560b8c5b4a2f2dbface1ca898b59e8f615cc
Alexej Kubarev <alexej.kubarev@accedo.tv>
2015-11-05 10:33:42 +0100

    Adding Go things

---
+++ go/env.zsh
@@ -0,0 +1,6 @@
+
+# GOPATH
+export GOPATH=$HOME/Personal/go
+
+# Add Go to PATH
+export PATH=$PATH:/usr/local/opt/go/libexec/bin:$GOPATH/bin
--- ruby/gem.zsh
+++ ruby/gem.zsh
@@ -1,3 +1,4 @@
 
 # Gems Home
 export GEM_HOME=/usr/local/Cellar/gems
+export PATH="$PATH:$GEM_HOME/bin"
