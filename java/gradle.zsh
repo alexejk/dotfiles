b286f9ec7a50f646ad2e9db2d3e9f3b13a81d187
Alexej Kubarev <alexej.kubarev@accedo.tv>
2015-11-19 17:11:15 +0100

    Setting back Java vars

--- java/gradle.zsh
+++ java/gradle.zsh
@@ -5,8 +5,8 @@ export GRADLE_HOME=/usr/local/opt/gradle/libexec
 #export GRADLE_OPTS="-Xss4m -Xmx2048m -Xms2048m"
 
 # Expose to GUI (on mac)
-#if [[ $OSTYPE == darwin* ]];
-#then
-#    launchctl setenv GRADLE_HOME "$GRADLE_HOME"
+if [[ $OSTYPE == darwin* ]];
+then
+    launchctl setenv GRADLE_HOME "$GRADLE_HOME"
 #    launchctl setenv GRADLE_OPTS "$GRADLE_OPTS"
-#fi
+fi
