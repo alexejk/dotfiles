7747b46a2283d36daa8b5fbe76a94bb6c0f01d03
Alexej Kubarev <info@codexp.net>
2016-11-22 22:58:09 +0100

    grc conditionals

--- system/colorize.zsh
+++ system/colorize.zsh
@@ -9,23 +9,27 @@ then
   then
     alias colourify="$GRC -es --colour=auto"
     alias configure='colourify ./configure'
-    alias diff='colourify diff'
-    alias make='colourify make'
-    alias gcc='colourify gcc'
-    alias g++='colourify g++'
-    alias as='colourify as'
-    alias gas='colourify gas'
-    alias ld='colourify ld'
-    alias netstat='colourify netstat'
+
+    # Some of these might not even be present!
+    if (( $+commands[diff] )); then; alias diff='colourify diff'; fi
+    if (( $+commands[make] )); then; alias make='colourify make'; fi
+    if (( $+commands[gcc] )); then; alias gcc='colourify gcc'; fi
+    if (( $+commands[g++] )); then; alias g++='colourify g++'; fi
+    if (( $+commands[as] )); then; alias as='colourify as'; fi
+    if (( $+commands[gas] )); then; alias gas='colourify gas'; fi
+    if (( $+commands[ld] )); then; alias ld='colourify ld'; fi
+    if (( $+commands[netstat] )); then; alias netstat='colourify netstat'; fi
+    if (( $+commands[traceroute] )); then; alias traceroute='colourify /usr/sbin/traceroute'; fi
+    if (( $+commands[head] )); then; alias head='colourify head'; fi
+    if (( $+commands[tail] )); then; alias tail='colourify tail'; fi
+    if (( $+commands[dig] )); then; alias dig='colourify dig'; fi
+    if (( $+commands[mount] )); then; alias mount='colourify mount'; fi
+    if (( $+commands[ps] )); then; alias ps='colourify ps'; fi
+    if (( $+commands[mtr] )); then; alias mtr='colourify mtr'; fi
+    if (( $+commands[df] )); then; alias df='colourify df'; fi
+
+    # Ping is always ping :)
     alias ping='colourify ping'
-    alias traceroute='colourify /usr/sbin/traceroute'
-    alias head='colourify head'
-    alias tail='colourify tail'
-    alias dig='colourify dig'
-    alias mount='colourify mount'
-    alias ps='colourify ps'
-    alias mtr='colourify mtr'
-    alias df='colourify df'
   fi
 fi
 
