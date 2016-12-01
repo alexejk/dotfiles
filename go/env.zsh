eb64e28c4d54fb2cc67962811e63cbfe5e1f1bf8
Alexej Kubarev <alexej.kubarev@accedo.tv>
2016-12-01 11:24:55 +0100

    Adding GOROOT variables

--- go/env.zsh
+++ go/env.zsh
@@ -1,6 +1,11 @@
+if (( $+commands[go] )) 
+then
+    export GOROOT=`go env GOROOT`
+fi
 
 # Expose to GUI (on mac)
 if [[ $OSTYPE == darwin* ]];
 then
     launchctl setenv GOPATH "$GOPATH"
+    launchctl setenv GOROOT "$GOROOT"
 fi
